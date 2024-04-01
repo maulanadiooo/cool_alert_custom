d88dbf2ecf10c617826b8896b7eac36fff821f70
Dio Maulana <maulanadiodm@gmail.com>
2024-04-01 15:50:30 +0700

    flutter cool alert

---
+++ lib/src/constants/images.dart
@@ -0,0 +1,10 @@
+class AppAnim {
+  static const loading = 'packages/cool_alert_custom/assets/flare/loading.flr';
+  static const success =
+      'packages/cool_alert_custom/assets/flare/success_check.flr';
+  static const error =
+      'packages/cool_alert_custom/assets/flare/error_check.flr';
+  static const warning =
+      'packages/cool_alert_custom/assets/flare/warning_check.flr';
+  static const info = 'packages/cool_alert_custom/assets/flare/info_check.flr';
+}
